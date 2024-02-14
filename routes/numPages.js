973ac92c7a93a801d7a8184738021e16d674c446
rikiPraizler <150063960+rikiPraizler@users.noreply.github.com>
2024-02-14 12:51:09 +0200

    Update numPages.js

--- routes/numPages.js
+++ routes/numPages.js
@@ -1,7 +1,7 @@
-import express from "express";
-import { getNumPages } from "../controllers/numPages.js";
+// import express from "express";
+// import { getNumPages } from "../controllers/numPages.js";
 
-const router = express.Router();
-router.get("/", getNumPages);
+// const router = express.Router();
+// router.get("/", getNumPages);
 
-export default router;
+// export default router;
