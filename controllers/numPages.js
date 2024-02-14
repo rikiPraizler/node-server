1591b32f074549a4f7de4c55eda43c758538a84c
rikiPraizler <150063960+rikiPraizler@users.noreply.github.com>
2024-02-14 12:54:16 +0200

    Update numPages.js

--- controllers/numPages.js
+++ controllers/numPages.js
@@ -1,12 +1,12 @@
-import { Bag } from '../models/bag.js';
+// import { Bag } from '../models/bag.js';
 
-export const getNumPages = async (req, res) => {
-    try {
-        let allBags = await Bag.countDocuments();
-        return res.send(allBags);
-    }
+// export const getNumPages = async (req, res) => {
+//     try {
+//         let allBags = await Bag.countDocuments();
+//         return res.send(allBags);
+//     }
 
-    catch (err) {
-        return res.status(400).send("an error occurred " + err);
-    }
-}
+//     catch (err) {
+//         return res.status(400).send("an error occurred " + err);
+//     }
+// }
